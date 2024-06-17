09be6faea02d867c91d6888209b75bd74a501ef4
twinkeee7 <167674604+twinkeee7@users.noreply.github.com>
2024-06-17 08:40:34 +0530

    Update mustjoin.py

--- PROFESSOR/plugins/bot/mustjoin.py
+++ PROFESSOR/plugins/bot/mustjoin.py
@@ -5,29 +5,15 @@ from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForb
 from PROFESSOR import app
 
 MISHI = [
-    "https://graph.org/file/f86b71018196c5cfe7344.jpg",
-    "https://graph.org/file/a3db9af88f25bb1b99325.jpg",
-    "https://graph.org/file/5b344a55f3d5199b63fa5.jpg",
-    "https://graph.org/file/84de4b440300297a8ecb3.jpg",
-    "https://graph.org/file/84e84ff778b045879d24f.jpg",
-    "https://graph.org/file/a4a8f0e5c0e6b18249ffc.jpg",
-    "https://graph.org/file/ed92cada78099c9c3a4f7.jpg",
-    "https://graph.org/file/d6360613d0fa7a9d2f90b.jpg"
-    "https://graph.org/file/37248e7bdff70c662a702.jpg",
-    "https://graph.org/file/0bfe29d15e918917d1305.jpg",
-    "https://graph.org/file/16b1a2828cc507f8048bd.jpg",
-    "https://graph.org/file/e6b01f23f2871e128dad8.jpg",
-    "https://graph.org/file/cacbdddee77784d9ed2b7.jpg",
-    "https://graph.org/file/ddc5d6ec1c33276507b19.jpg",
-    "https://graph.org/file/39d7277189360d2c85b62.jpg",
-    "https://graph.org/file/5846b9214eaf12c3ed100.jpg",
-    "https://graph.org/file/ad4f9beb4d526e6615e18.jpg",
-    "https://graph.org/file/3514efaabe774e4f181f2.jpg",
+    "https://telegra.ph/file/c2ff050b35f2a1aeecc09.jpg",
+    "https://telegra.ph/file/c2ff050b35f2a1aeecc09.jpg",
+    "https://telegra.ph/file/463dd0acf84d4b1ae10b0.jpg",
+    "https://telegra.ph/file/e2cba3be8cc93a534435f.jpg",
 ]
 
 #--------------------------
 
-MUST_JOIN = "Friends_Chatting_Masti_Group"
+MUST_JOIN = "https://t.me/+wE8H1Xl6NnBiNzg9"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
@@ -43,11 +29,11 @@ async def must_join_channel(app: Client, msg: Message):
                 chat_info = await app.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
             try:
-                await msg.reply_photo(random.choice(MISHI), caption=f"❅ ʜᴇʏ ᴛʜᴇʀᴇ, ɴɪᴄᴇ ᴛᴏ ᴍᴇᴇᴛ ᴜʜʜ !\n\n❅ ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜsᴇ ᴘʀᴏғᴇssᴏʀ ʙᴏᴛ, ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴ ᴀɴᴅ ʏᴏᴜ ᴊᴏɪɴᴇᴅ, ᴛʜᴇɴ ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴀʟʟ ᴍʏ ᴄᴏᴍᴍᴀɴᴅs ",
+                await msg.reply_photo(random.choice(MISHI), caption=f"❅ Hᴇʏ I ᴀᴍ Rɪsᴇ ғɪʀsᴛ ᴜɴ Jɪᴏɴ ᴛʜᴇ ɢʀᴏᴜᴘ ᴛʜᴇɴ sᴛᴀʀᴛ ᴛʜᴇ Bᴏᴛ OK ᴛʜᴀɴᴋʏᴏᴜ  ",
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇ", url="https://t.me/PROFESSOR_NETWORK"),
+                                InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇ", url="https://t.me/DevilCommunityyy"),
                                 InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=link),
                             ]
                         ]
